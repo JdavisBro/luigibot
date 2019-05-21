2c69e29c5508896092b890f487ad0fefb2717044
JdavisBro <44114870+JdavisBro@users.noreply.github.com>
2019-05-21 02:00:58 +0100

    Update luigi.py

--- luigi.py
+++ luigi.py
@@ -40,7 +40,7 @@ client.origauthor = {}
 
 @client.event
 async def on_ready():
-    await asyncio.sleep(10)
+    await asyncio.sleep(5)
     logging.info('Connected to DISCORD as {}'.format(str(client.user)))
     game = discord.Game(name='with my LuigiBoard.')
     await client.change_presence(status=discord.Status.online, activity=game)
